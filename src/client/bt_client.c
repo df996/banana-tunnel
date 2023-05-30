4ac173c30da0a5840cad4dd4bf3e41230e562a31
dafeng996 <1236192@qq.com>
2023-05-30 11:16:50 +0800

    :fire:

---
+++ src/client/bt_client.c
@@ -0,0 +1,10 @@
+//
+// Created by cheng on 2023/5/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("client start ...");
+    return 0;
+}
---
+++ src/server/bt_server.c
@@ -0,0 +1,10 @@
+//
+// Created by cheng on 2023/5/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("server start...");
+    return 0;
+}
