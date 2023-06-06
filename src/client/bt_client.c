a66b28553b4659126a6a3acc3533b8895846131f
dafeng996 <1236192@qq.com>
2023-06-06 18:12:30 +0800

    asd

--- src/client/bt_client.c
+++ src/client/bt_client.c
@@ -6,13 +6,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[]) {
-    char *str = "127.0.0.1:3306";
-    char *val = strtok(str, ":");
-    while (val != NULL) {
-        printf("val: %s\n", val);
-        val = strtok(NULL, ":");
+/**
+ * ip端口信息
+ */
+struct ip_port {
+    char *ip;
+    char *port;
+};
+
+/**
+ * 分割ip:port字符串
+ * @param str
+ * @return 返回ip_port结构体, 如果格式错误返回NULL
+ */
+struct ip_port *split_ip_port(char *str) {
+    struct ip_port *ip_port = (struct ip_port *) (struct srv_ip_port *)
+            malloc(sizeof(struct ip_port));
+
+    char *index = strchr(str, ':');
+    if (index == NULL) {
+        return NULL;
     }
+
+    return ip_port;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc <= 2) {
+        printf("Usage: bt-client server:port ports...\n");
+        exit(1);
+    }
+
+    struct ip_port *srv_ip_port = split_ip_port(argv[1]);
+    if (srv_ip_port == NULL) {
+        printf("Usage: bt-client server:port ports...\n");
+        exit(1);
+    }
+
+    // char *index = strchr(argv[1], ':');
+    // if (index == NULL) {
+    //     printf("Usage: bt-client server:port ports...\n");
+    //     exit(1);
+    // }
+
+    // char *srv_ip, *srv_port, *ptr;
+    // char *val = strtok_s(argv[1], ":", &ptr);
+    // srv_ip = (char *) malloc(strlen(val) + 1);
+    // strcpy_s(srv_ip, strlen(val) + 1, val);
+    // val = strtok_s(NULL, ":", &ptr);
+    // srv_port = (char *) malloc(strlen(val) + 1);
+    // strcpy_s(srv_port, strlen(val) + 1, val);
+
+    // printf("IP: %s, PORT: %s", srv_ip, srv_port);
+    return 0;
 
     // if (argc <= 2) {
     //     printf("Usage: bt-client server-ip:port ports...\n");
