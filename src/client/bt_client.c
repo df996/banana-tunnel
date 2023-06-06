bd5f0d3f92deb50e97509e84fa159224fc4baffe
王成 <1236192@qq.com>
2023-06-06 11:37:06 +0800

    :fire:

--- src/client/bt_client.c
+++ src/client/bt_client.c
@@ -7,10 +7,17 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-    if (argc <= 2) {
-        printf("Usage: bt-client server-ip ports...\n");
-        exit(1);
+    char *str = "127.0.0.1:3306";
+    char *val = strtok(str, ":");
+    while (val != NULL) {
+        printf("val: %s\n", val);
+        val = strtok(NULL, ":");
     }
+
+    // if (argc <= 2) {
+    //     printf("Usage: bt-client server-ip:port ports...\n");
+    //     exit(1);
+    // }
 
     // int index = 0;
     // char **arg = argv;
@@ -24,23 +31,23 @@ int main(int argc, char* argv[]) {
     //     printf("%s, %p\n", *arg, arg);
     // }
 
-    int count = 0; 
-    while(argv[++count] != NULL);
-    printf("argv length: %d\n", count);
-    printf("argv length: %d\n", sizeof(argv) / sizeof(*argv));
+    // int count = 0; 
+    // while(argv[++count] != NULL);
+    // printf("argv length: %d\n", count);
+    // printf("argv length: %d\n", sizeof(argv) / sizeof(*argv));
 
     // char **ports;
     // ports = malloc(sizeof(char *) * )
 
-    int index = 0;
-    char **arg = NULL;
-    for (arg = argv; *arg != NULL; arg++) {
-        index++;
-        if (index < 2) {
-            continue;
-        }
-        printf("string: %s length: %d pinter: %llu\n", *arg, strlen(*arg), arg);
-    }
+    // int index = 0;
+    // char **arg = NULL;
+    // for (arg = argv; *arg != NULL; arg++) {
+    //     index++;
+    //     if (index < 2) {
+    //         continue;
+    //     }
+    //     printf("string: %s length: %d pinter: %llu\n", *arg, strlen(*arg), arg);
+    // }
 
     return 0;
 }
